818e3d2bd2c5934003fdecbb58603112f8facb0c
rennavittorio <renna.vittorio@gmail.com>
2023-03-31 14:57:34 +0200

    add: add doghouse class add: add toy class

--- Models/Doghouse.php
+++ Models/Doghouse.php
@@ -1,1 +1,38 @@
 <?php
+
+class Doghouse extends Product
+{
+    protected $prodLength;
+    protected $prodHeight;
+
+    function __construct($prefix, $_img, $_title, $_price, $_prodLength, $_prodHeight)
+    {
+        Product::__construct($prefix, $_img, $_title, $_price);
+
+        $this->setProdLength($_prodLength);
+        $this->setProdHeight($_prodHeight);
+    }
+
+
+    //setters
+    public function setProdLength($newValue)
+    {
+        $this->prodLength = $newValue;
+    }
+
+    public function setProdHeight($newValue)
+    {
+        $this->prodHeight = $newValue;
+    }
+
+    //getters
+    public function getProdLength()
+    {
+        return $this->prodLength;
+    }
+
+    public function getProdHeight()
+    {
+        return $this->prodHeight;
+    }
+}
--- Models/Toy.php
+++ Models/Toy.php
@@ -1,1 +1,38 @@
 <?php
+
+class Toy extends Product
+{
+    protected $isForPuppies;
+    protected $isPlasticFree;
+
+    function __construct($prefix, $_img, $_title, $_price, $_isForPuppies, $_isPlasticFree)
+    {
+        Product::__construct($prefix, $_img, $_title, $_price);
+
+        $this->setIsForPuppies($_isForPuppies);
+        $this->setIsPlasticFree($_isPlasticFree);
+    }
+
+
+    //setters
+    public function setIsForPuppies($newValue)
+    {
+        $this->isForPuppies = $newValue;
+    }
+
+    public function setIsPlasticFree($newValue)
+    {
+        $this->isPlasticFree = $newValue;
+    }
+
+    //getters
+    public function getIsForPuppies()
+    {
+        return $this->isForPuppies;
+    }
+
+    public function getIsPlasticFree()
+    {
+        return $this->isPlasticFree;
+    }
+}
